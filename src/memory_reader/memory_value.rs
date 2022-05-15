e8c8e1030a3e94ba2ad8ccd55a7a04aa227114b5
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-05-15 08:55:02 -0500

    Removed commented code

--- src/memory_reader/memory_value.rs
+++ src/memory_reader/memory_value.rs
@@ -18,66 +18,6 @@ impl<T> MemoryValue<T> {
     }
 }
 
-// pub trait CollectBytes<U>: Iterator<Item = MemoryValue<U>>
-// where
-//     U: Into<u8>,
-// {
-//     fn iter_byte_arr<const N: usize>(self) -> ByteCollector<Self, U, N>
-//     where
-//         Self: Sized,
-//     {
-//         ByteCollector::new(self)
-//     }
-// }
-
-// impl<T, U> CollectBytes<U> for T
-// where
-//     T: Iterator<Item = MemoryValue<U>>,
-//     U: Into<u8>,
-// {
-// }
-
-// pub struct ByteCollector<I, U, const N: usize>
-// where
-//     I: Iterator<Item = MemoryValue<U>>,
-//     U: Into<u8>,
-// {
-//     iter: I,
-// }
-
-// impl<I, U, const N: usize> ByteCollector<I, U, N>
-// where
-//     I: Iterator<Item = MemoryValue<U>>,
-//     U: Into<u8>,
-// {
-//     fn new(iter: I) -> Self {
-//         Self { iter }
-//     }
-// }
-
-// impl<I, U, const N: usize> Iterator for ByteCollector<I, U, N>
-// where
-//     I: Iterator<Item = MemoryValue<U>>,
-//     U: Into<u8>,
-// {
-//     type Item = MemoryValue<[u8; N]>;
-//     fn next(&mut self) -> Option<Self::Item> {
-//         let mut location = Pointer::null();
-//         let mut value = [0; N];
-//         for i in 0..N {
-//             if let Some(val) = self.iter.next() {
-//                 if i == 0 {
-//                     location = val.location;
-//                 }
-//                 value[i] = val.value.into();
-//             } else {
-//                 return None;
-//             }
-//         }
-//         Some(Self::Item { location, value })
-//     }
-// }
-
 pub trait CollectBytes: Iterator<Item = MemoryValue<u8>> {
     fn iter_byte_arr<const N: usize>(self) -> ByteCollector<Self, N>
     where
