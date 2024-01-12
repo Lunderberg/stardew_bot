d4de778621caa6a502058a18d6eb4860905e50bd
Lunderberg <eldritch.cheese@gmail.com>
2024-01-11 19:40:44 -0600

    Remove commented-out code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-//use itertools::Itertools;
-
 use stardew_bot::{Error, Result, TuiExplorer};
 
 use sysinfo::{PidExt, ProcessExt, SystemExt};
@@ -23,76 +21,6 @@ fn main() -> Result<()> {
 
     let mut tui = TuiExplorer::new(pid)?;
     tui.run()?;
-
-    // let reader = MemoryReader::new(pid)?;
-
-    // println!(
-    //     "Total memory: {} MB",
-    //     (reader.total_memory() as f32) / (1024.0 * 1024.0)
-    // );
-
-    // println!(
-    //     "Total readable memory: {} MB",
-    //     (reader.total_readable_memory() as f32) / (1024.0 * 1024.0)
-    // );
-
-    // println!(
-    //     "Total writable memory: {} MB",
-    //     (reader.total_writable_memory() as f32) / (1024.0 * 1024.0)
-    // );
-
-    // println!(
-    //     "Stack size: {} MB",
-    //     (reader.stack()?.size_bytes() as f32) / (1024.0 * 1024.0)
-    // );
-
-    // reader
-    //     .potential_frame_pointers()?
-    //     .into_iter()
-    //     .sorted_by_key(|ptr_ptr| ptr_ptr.location)
-    //     .for_each(|ptr_ptr| {
-    //         let pointer_from = ptr_ptr.location;
-    //         let pointer_to = ptr_ptr.value;
-    //         let direction = if pointer_to > pointer_from { "+" } else { "-" };
-    //         let abs_delta = if pointer_to > pointer_from {
-    //             pointer_to - pointer_from
-    //         } else {
-    //             pointer_from - pointer_to
-    //         };
-    //         println!(
-    //             "Pointer at {} points to {}, delta = {}{}",
-    //             pointer_from, pointer_to, direction, abs_delta
-    //         );
-    //     });
-
-    // let num_ptr_executable =
-    //     reader.potential_return_instruction_pointers()?.count();
-    // println!(
-    //     "There exist {} pointers to executable space in the [stack]",
-    //     num_ptr_executable
-    // );
-
-    // reader.potential_return_instruction_pointers()?.for_each(
-    //     |(ptr_ptr, region)| {
-    //         println!(
-    //             "Pointer at {} points to {}, located in {:?}",
-    //             ptr_ptr.location, ptr_ptr.value, region.name
-    //         );
-    //     },
-    // );
-
-    // reader
-    //     .pointers_in_stack_with_region()?
-    //     .for_each(|(ptr_ptr, region)| {
-    //         let name =
-    //             region.name.as_ref().map(|p| p.as_str()).unwrap_or("???");
-    //         println!(
-    //             "Pointer at {} points to {}, located in {}",
-    //             ptr_ptr.location, ptr_ptr.value, name
-    //         );
-    //     });
-
-    // reader.print_stack()?;
 
     Ok(())
 }
